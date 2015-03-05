9a79834d4fedca40025b4f4f02c6e0f0e3889185
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-03-05 16:55:37 +0200

    =dsd

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<html>
-	<head>
-		
-	</head>
-	<body>
-		MOTHER FUCKERS 
+<?php 
+$info = array('ahmed',22,'Mansoura','010329329');
+echo "My name is $info[0]<br>";
+echo "My age is $info[1]<br>";
+echo "im from $info[2]<br>";
+echo "My No is $info[3]";
 
-		Kfkf
-	</body>
-</html>
+
+ ?>
